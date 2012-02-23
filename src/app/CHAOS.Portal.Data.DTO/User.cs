f861dbee92409bdd20f1f97f6770464867177138
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2012-02-23 17:09:10 +0100

    Added: Data Transfer Object project, to replace current LINQ based DTOs.

---
+++ src/app/CHAOS.Portal.Data.DTO/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Geckon.Portal.Data.Result.Standard;
+
+namespace CHAOS.Portal.Data.DTO
+{
+	public class User : Result
+	{
+
+	}
+}
