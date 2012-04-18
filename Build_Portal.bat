35dbc86cc6bb5183d15aa8ee1f9a0e3c70a7bc66
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2012-04-18 14:02:54 +0200

    New Build scripts

---
+++ Build_Portal.bat
@@ -0,0 +1,14 @@
+@echo off
+
+rem Needed to update variable in loop
+setlocal enabledelayedexpansion
+
+for %%i in (.\bin\Portal\*.dll) do (set files=!files!%%~i )
+
+echo Merging Portal SDK
+
+tools\ILMerge\ILMerge.exe /out:build\Portal.dll /lib:C:\Windows\Microsoft.NET\Framework64\v4.0.30319 /targetplatform:v4,C:\Windows\Microsoft.NET\Framework64\v4.0.30319 /lib:"C:\Program Files (x86)\Microsoft ASP.NET\ASP.NET MVC 3\Assemblies" /lib:lib\ %files%
+
+echo Done
+
+pause
