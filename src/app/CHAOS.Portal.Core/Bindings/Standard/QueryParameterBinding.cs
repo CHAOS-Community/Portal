3b9e2a39f730b00f0ed99811860a91371f88b124
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2012-06-25 15:49:01 +0200

    Added new Index implementation (solr) and updated QueryBinding to support Facets

--- src/app/CHAOS.Portal.Core/Bindings/Standard/QueryParameterBinding.cs
+++ src/app/CHAOS.Portal.Core/Bindings/Standard/QueryParameterBinding.cs
@@ -9,14 +9,18 @@ namespace CHAOS.Portal.Core.Bindings.Standard
         {
             var qs        = callContext.PortalRequest.Parameters;
             var query     = qs.ContainsKey("query") ? qs["query"] : null;
+            var facet     = qs.ContainsKey("facet") ? qs["facet"] : null;
             var sort      = qs.ContainsKey("sort")  ? qs["sort"] : null;
             var pageIndex = qs.ContainsKey("pageIndex") ? qs["pageIndex"] : null;
             var pageSize  = qs.ContainsKey("pageSize") ? qs["pageSize"] : null;
 
-            if( string.IsNullOrEmpty( query ) || string.IsNullOrEmpty( pageIndex ) || string.IsNullOrEmpty( pageSize ) )
+            if( string.IsNullOrEmpty( query ) )
+                query = "*:*";
+
+            if( string.IsNullOrEmpty( pageIndex ) || string.IsNullOrEmpty( pageSize ) )
                 return null;
 
-            return new SolrQuery( query, sort, uint.Parse( pageIndex ), uint.Parse( pageSize ) );
+            return new SolrQuery( query, facet, sort, uint.Parse( pageIndex ), uint.Parse( pageSize ) );
         }
     }
 }
