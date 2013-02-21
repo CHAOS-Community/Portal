86650ddbfbc65cb485a9ac7762c9bf94c9272fb9
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2013-02-21 18:22:02 +0100

    Added Cache test and configration to app.config.sample

---
+++ src/test/Chaos.Portal.IntegrationTest/Cache/Couchbase/CacheTest.cs
@@ -0,0 +1,25 @@
+﻿namespace Chaos.Portal.IntegrationTest.Cache.Couchbase
+{
+    using NUnit.Framework;
+
+    using Chaos.Portal.Cache.Couchbase;
+
+    using global::Couchbase;
+
+    [TestFixture]
+    public class CacheTest
+    {
+        #region Setup
+
+        private Cache _cache;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var client = new CouchbaseClient();
+            _cache     = new Cache(client);
+        }
+
+        #endregion
+    }
+}
