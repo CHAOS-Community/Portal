40218c3bcc68183bba863c277442be7719b57795
Jacob Poul Richardt <spam@poulrichardt.dk>
2013-05-13 10:27:21 +0200

    Update v6 Group tests.

--- src/test/Chaos.Portal.Protocol.Tests/v6/Extension/GroupTest.cs
+++ src/test/Chaos.Portal.Protocol.Tests/v6/Extension/GroupTest.cs
@@ -35,8 +35,9 @@
 			var groups = new[] { Make_Group(), Make_Group() };
 
 			currentUser.SystemPermissonsEnum = SystemPermissons.None;
-			PortalRepository.Setup(m => m.UserInfoGet(null, It.Is<Guid?>(item => item.HasValue), null)).Returns(new[] { currentUser });
-			PortalRepository.Setup(m => m.GroupGet(null, null, It.Is<Guid?>(item => item.HasValue))).Returns(groups);
+			
+			PortalRequest.SetupGet(p => p.User).Returns(currentUser);
+			PortalRequest.SetupGet(p => p.Groups).Returns(groups);
 			PortalRequest.SetupGet(m => m.Parameters).Returns(new Dictionary<string, string>() { { "sessionGUID", Make_Session().Guid.ToString() } });
 
 			var results = group.Get();
@@ -44,6 +45,25 @@
 			Assert.That(results.Count(), Is.EqualTo(2));
 			Assert.That(results.First(), Is.EqualTo(groups.First()));
 			PortalRepository.Verify(m => m.GroupGet(null, null, null), Times.Never());
+		}
+
+		[Test]
+		public void Delete_WithAdminSystemPermission_DeleteGroup()
+		{
+			var group = Make_v6GroupExtension();
+			var currentUser = Make_User();
+			var groupGuid = Guid.NewGuid();
+
+			currentUser.SystemPermissonsEnum = SystemPermissons.All;
+
+			PortalRequest.SetupGet(p => p.User).Returns(currentUser);
+			PortalRequest.SetupGet(m => m.Parameters).Returns(new Dictionary<string, string>() { { "sessionGUID", Make_Session().Guid.ToString() } });
+
+			PortalRepository.Setup(p => p.GroupDelete(groupGuid, currentUser.Guid)).Returns(1);
+
+			var result = group.Delete(groupGuid);
+
+			Assert.That(result.Value, Is.EqualTo(1));
 		}
 	}
 }
