e04c191fec28227251d305cb6ed50186b8d1718b
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2013-08-09 18:11:08 +0200

    Added GroupedResult as a return type from an extension

---
+++ src/app/Chaos.Portal.Core/Response/Dto/GroupedResult.cs
@@ -0,0 +1,33 @@
+﻿namespace Chaos.Portal.Core.Response.Dto
+{
+    using System.Collections.Generic;
+
+    using CHAOS.Serialization;
+
+    using Chaos.Portal.Core.Data.Model;
+
+    [Serialize("GroupedResult")]
+    public class GroupedResult<TResult> : IResult where TResult : IResult
+    {
+        #region Properties
+
+        [Serialize("Groups")]
+        public IList<ResultGroup<TResult>> Groups { get; set; }
+
+        #endregion
+        #region Initialization
+
+        public GroupedResult()
+        {
+            Groups = new List<ResultGroup<TResult>>();
+        }
+
+        #endregion
+
+        #region Implementation of IResult
+
+        public string Fullname { get { return "Chaos.Portal.Core.Response.Dto.GroupedResult"; } }
+
+        #endregion
+    }
+}
