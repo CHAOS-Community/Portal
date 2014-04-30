53925f9589d7c17ef40777d9f1affd112326b033
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2014-04-30 17:14:03 +0100

    Added ContentLength to Attachment, so download progress can be displayed in the browser

--- src/app/Chaos.Portal.Core/Data/Model/Attachment.cs
+++ src/app/Chaos.Portal.Core/Data/Model/Attachment.cs
@@ -7,6 +7,7 @@ namespace Chaos.Portal.Core.Data.Model
     {
         public string FileName { get; set; }
         public string ContentType { get; set; }
+        public ulong ContentLength { get; set; }
         public IDisposable Disposable { get; set; }
         public Stream Stream { get; set; }
 		public bool AsAttachment { get; set; }
