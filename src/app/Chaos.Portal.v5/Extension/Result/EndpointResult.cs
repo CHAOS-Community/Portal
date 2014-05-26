62d5c3281f4f0c4cf293139b731c511fd8e8e423
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2014-05-26 14:38:58 +0100

    Added EndpointResult Result classs.

---
+++ src/app/Chaos.Portal.v5/Extension/Result/EndpointResult.cs
@@ -0,0 +1,21 @@
+﻿namespace Chaos.Portal.v5.Extension.Result
+{
+    using CHAOS.Serialization;
+    using Core.Data.Model;
+
+    public class EndpointResult : AResult
+    {
+        [Serialize]
+        public bool WasSuccess { get; set; }
+
+        public static EndpointResult Success()
+        {
+            return new EndpointResult { WasSuccess = true };
+        }
+
+        public static EndpointResult Failed()
+        {
+            return new EndpointResult { WasSuccess = false };
+        }
+    }
+}
