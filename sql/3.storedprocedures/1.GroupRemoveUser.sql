31a3148ae2969ddb9c818bc5d16d66ddcd839f19
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2015-01-19 13:55:41 +0000

    Removed ErrorCode out param from GroupRemove Stored Procedure

--- sql/3.storedprocedures/1.GroupRemoveUser.sql
+++ sql/3.storedprocedures/1.GroupRemoveUser.sql
@@ -1,21 +1,20 @@
 CREATE PROCEDURE `Group_RemoveUser`(
     IN  GroupGUID       BINARY(16),
     IN  UserGUID        BINARY(16),
-    IN  RequestUserGUID BINARY(16),
-    OUT ErrorCode       INTEGER
+    IN  RequestUserGUID BINARY(16)
 )
 BEGIN
 
     IF RequestUserGUID IS NULL OR DoesUserHavePermissionToGroup( RequestUserGUID, GroupGUID, 'REMOVE_USER' ) = 1 THEN
 		
-	DELETE 
-	FROM Group_User_Join 
-	WHERE Group_User_Join.GroupGUID = GroupGUID AND Group_User_Join.UserGUID = UserGUID 
-	LIMIT 1;
+		DELETE 
+		FROM Group_User_Join 
+		WHERE Group_User_Join.GroupGUID = GroupGUID AND Group_User_Join.UserGUID = UserGUID 
+		LIMIT 1;
              
-        SET ErrorCode = 1;
+        SELECT 1;
     ELSE
-        SET ErrorCode = -100;
+        SELECT -100;
     END IF;
 
 END
